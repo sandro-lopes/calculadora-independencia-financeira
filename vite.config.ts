f909d4e495c370e9d5233c1227804a945cf172c2
Sandro Lopes <sandrolopes6@gmail.com>
2026-02-07 17:28:54 -0300

    correção base do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: 'https://github.com/sandro-lopes/calculadora-independencia-financeira',
+  base: '/calculadora-independencia-financeira',
   plugins: [react()],
   resolve: {
     alias: {
